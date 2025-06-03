49182984dd00b8702f191c1c06059ddf5aa126cd
mukilan2815 <mukilan2808@gmail.com>
2025-06-03 15:42:56 +0530

    Fix voucher ID prefix format by removing trailing hyphen

--- controllers/adminReceiptController.js
+++ controllers/adminReceiptController.js
@@ -7,7 +7,7 @@ const generateVoucherId = async () => {
   const date = new Date();
   const year = date.getFullYear().toString().substr(-2);
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const prefix = `WK-`;
+  const prefix = `WK`;
 
   // Find the latest receipt with this prefix
   const latestReceipt = await AdminReceipt.findOne({
